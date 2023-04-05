003430a917fcd22551d83cab0de508a8fa8e4626
Mario Krajacic <krajacic.mario@gmail.com>
2023-04-05 19:21:33 +0200

    Adding ZStack with my essential details

--- My Business Card/ContentView.swift
+++ My Business Card/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack
+        {
+            Color(red: 0.16, green: 0.50, blue: 0.73)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Mario Krajačić")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.white)
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
