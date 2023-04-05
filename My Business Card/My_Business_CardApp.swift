7e86ee76be131eef1007c32bda6e976c00c95ed3
Mario Krajacic <krajacic.mario@gmail.com>
2023-04-05 15:47:07 +0200

    Initial Commit

---
+++ My Business Card/My_Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Business_CardApp.swift
+//  My Business Card
+//
+//  Created by Mario Krajačić on 05.04.2023..
+//
+
+import SwiftUI
+
+@main
+struct My_Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
